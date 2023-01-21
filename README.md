835c62172e13108e9bfb9e421e01d260f529bd35
Rajesh Gour <rajeshlnctbpl@gmail.com>
2023-01-21 21:44:13 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+note down information here
---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s1.png',
+            price: 150,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: 'Slim Fit Shirt',
+            category: 'Shirts',
+            image: '/images/s2.png',
+            price: 160,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s3.png',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s4.png',
+            price: 130,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '5',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s5.png',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '6',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s6.png',
+            price: 200,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+    ]
+}
